08a3fc4cc65dab6ef8c726a36c998360b9e9a140
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-05-24 13:27:31 +0300

    Update to the .NET MAUI GA

--- templates/OnionArchitectureCrossPlatformApplication/src/UI/Mobile/App1.Mobile/MauiProgram.cs
+++ templates/OnionArchitectureCrossPlatformApplication/src/UI/Mobile/App1.Mobile/MauiProgram.cs
@@ -14,7 +14,12 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder.UseMauiApp<App>();
+		builder.UseMauiApp<App>()
+		       .ConfigureFonts(fonts =>
+		       {
+			       fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			       fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			   });
 		builder.UseMauiCommunityToolkit();
 		builder.Services.AddApplication();
 		builder.Services.AddInfrastructureData(GetDatabaseConnectionString("App1"));
